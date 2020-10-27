6e7657830af8304ab8ca30caeb87e0946a1e15a6
Rachel Cross <racross1@gmail.com>
2020-10-27 20:53:40 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(array, string)
+  array.push(string)
+end
+  
+using_unshift
+  takes in two arguments, an array and a string and adds that string to the front of the array using the unshift method (FAILED - 2)
+  increases the length of the array (FAILED - 3)
+
+using_pop
+  takes in argument of an array and uses the pop method to remove the last element from the array and return that element (FAILED - 4)
+  decreases the length of the array by 1 (FAILED - 5)
+
+pop_with_args
+  takes in an argument of an array and uses the pop method with an argument of 2 to remove the last two array items and return them (FAILED - 6)
+  decreases the length of the array by 2 (FAILED - 7)
+
+using_shift
+  takes in an argument of an array and uses the shift method to remove the first item and return it (FAILED - 8)
+  decreases the length of the array by 1 (FAILED - 9)
+
+shift_with_args
+  takes in an argument of an array and uses the shift method with an argument of 2 to remove and return the first 2 items from the array (FAILED - 10)
+  decreases the length of the array by 2 (FAILED - 11)
